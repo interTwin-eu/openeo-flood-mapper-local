5d175e3c030df8587616b4c69ab3e4e3275ce05e
Martin Schobben <martin.schobben@geo.tuwien.ac.at>
2023-11-29 16:02:16 +0100

    masking outliers + conflicting distributions +  incidence angles

--- src/openeo_flood_mapper_local/restructure_plia.py
+++ src/openeo_flood_mapper_local/restructure_plia.py
@@ -10,7 +10,7 @@ from eotransform_pandas.filesystem.naming.geopathfinder_conventions import yeoda
 DATA_VERSION = 'V01R03'
 
 
-def restructure_plia(root: Path, out: Path, tile_long_name: str) -> None:
+def restructure_plia(root: Path, out: Path, tile_long_name: str, tag: str) -> None:
     grid, tile = tile_long_name.split('_')
     grid = f'EQUI7_{grid}'
     plia_df = gather_files(root, yeoda_naming_convention, [re.compile('PLIA-TAG'),
@@ -19,7 +19,7 @@ def restructure_plia(root: Path, out: Path, tile_long_name: str) -> None:
                                                              re.compile(tile)], index='extra_field')
     out_dir = out / DATA_VERSION / grid / tile
     out_dir.mkdir(parents=True, exist_ok=True)
-
+    plia_df = plia_df.filter(like=tag, axis=0)
     for _, row in plia_df.iterrows():
         file = row['filepath']
         da = rioxarray.open_rasterio(file, mask_and_scale=True, chunks=500).squeeze().drop_vars("band")
@@ -30,15 +30,15 @@ def restructure_plia(root: Path, out: Path, tile_long_name: str) -> None:
             da.encoding.update({'scale_factor': 0.01, '_FillValue': -9999, 'dtype': 'int16', 'zlib': True})
         da.to_dataset(name='PLIA').to_netcdf(out_dir / f"{file.stem}.nc", mode='w')
 
-
 def main():
     parser = argparse.ArgumentParser(description="Restructure plia tiffs to be loaded as local openEO dataset.")
     parser.add_argument('root', type=Path, help="root path to the yeoda file structure")
     parser.add_argument('out', type=Path, help="root path to output structure")
     parser.add_argument('tile', type=str, help='long name of tile to process, i.e. "EU020M_E051N015T3"')
+    parser.add_argument('tag', type=str, help='tag of ... to process, i.e. "D080"')
     args = parser.parse_args()
 
-    restructure_plia(args.root, args.out, args.tile)
+    restructure_plia(args.root, args.out, args.tile, args.tag)
 
 
 if __name__ == '__main__':
