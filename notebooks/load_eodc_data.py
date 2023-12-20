131b7b9fcec976be1d987526372d2f0bafe1c193
Martin Schobben <martin.schobben@geo.tuwien.ac.at>
2023-12-20 17:18:03 +0100

    add mask method

--- notebooks/load_eodc_data.py
+++
@@ -1,8 +0,0 @@
-import openeo
-backend = "https://openeo.eodc.eu"
-
-connection = openeo.connect(backend)
-connection.authenticate_oidc(provider_id='egi')
-connection.list_collection_ids()
-connection.list_collections()
-connection.list_processes()
